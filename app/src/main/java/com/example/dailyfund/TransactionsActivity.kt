d3ad8eb72d3cbddec6be7f944a9e6a87ec0d19a0
Lilio <nabil.marachly05@gmail.com>
2024-11-21 22:38:48 +0100

    in development of adding transaction. crash when going to the "transaction" page

--- app/src/main/java/com/example/dailyfund/TransactionsActivity.kt
+++ app/src/main/java/com/example/dailyfund/TransactionsActivity.kt
@@ -3,6 +3,7 @@ package com.example.dailyfund
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
 class TransactionsActivity : AppCompatActivity() {
@@ -10,6 +11,11 @@ class TransactionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transactions)
+
+        val transactionList = findViewById<ListView>(R.id.ListTransactions)
+        val postsArray = arrayListOf("1","2","3","4","5")
+        val adapter = transactionsAdapter(this, R.layout.item_transaction, postsArray)
+        transactionList.adapter = adapter
 
     }
 
